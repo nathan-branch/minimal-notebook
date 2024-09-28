4b759a30964d2db0237897aea32c630c05af041f
Nathan Branch <148912472+nathan-branch@users.noreply.github.com>
2024-09-28 18:17:56 -0500

    init

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+{
+    "name": "Pocket Notebook",
+    "short_name": "Notebook",
+    "start_url": "/index.html",
+    "display": "standalone",
+    "background_color": "#f2f2f2",
+    "theme_color": "#ffffff",
+    "description": "A minimalist pocket notebook app.",
+    "icons": [
+      {
+        "src": "icon.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      }
+    ]
+  }
